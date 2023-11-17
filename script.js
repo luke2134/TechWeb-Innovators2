e204d86f84d66d018edd92b6382ed7c1430a920a
luke2134 <neverending.21x@example.com>
2023-11-17 14:54:31 -0500

    Initial commit: project setup

---
+++ script.js
@@ -0,0 +1,17 @@
+function createObject() {
+    // Get form data and perform create operation
+    alert('Object created successfully!');
+    // Redirect or perform other actions as needed
+}
+
+function updateObject() {
+    // Get form data and perform update operation
+    alert('Object updated successfully!');
+    // Redirect or perform other actions as needed
+}
+
+function deleteObject() {
+    // Perform delete operation
+    alert('Object deleted successfully!');
+    // Redirect or perform other actions as needed
+}
